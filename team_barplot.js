1056e16fccd633a8833b7d94e3f528aabd5b567b
Krzysztof Garbowicz <58812421+KrzysztofGarbowicz@users.noreply.github.com>
2021-12-16 23:46:11 +0100

    update

--- team_barplot.js
+++ team_barplot.js
@@ -148,7 +148,7 @@ var drawGraph = function(fileName = selectedFolder, teamName = selectedTeam) {
     
 
     // Animation
-    svg.selectAll("rect")
+    svg.select("mybar").selectAll("rect")
         .transition()
         .duration(800)
         .attr("y", function(d) { return y(d.NumberOfShots); })
--- team_barplot2.js
+++ team_barplot2.js
@@ -152,12 +152,11 @@ var drawGraph2 = function(fileName2 = selectedFolder2, teamName2 = selectedTeam2
     //     })
 
         // Animation
-    svg2.selectAll("rect")
+    svg2.select("mybar2").selectAll("rect")
         .transition()
         .duration(800)
         .attr("y", function(d) { return y2(d.NumberOfShots); })
         .attr("height", function(d) { return height2 - y2(d.NumberOfShots); })
-        .delay(function(d,i){console.log(i) ; return(i*100)})
 
     })  
 
