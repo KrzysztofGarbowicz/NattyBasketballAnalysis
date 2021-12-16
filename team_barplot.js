20607ab5e5c01ece8e3e1e0cea0a5e6c3e418309
Krzysztof Garbowicz <58812421+KrzysztofGarbowicz@users.noreply.github.com>
2021-12-16 01:22:53 +0100

    update

--- team_barplot.js
+++ team_barplot.js
@@ -97,7 +97,7 @@ svg.append("g")
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([0, team_range[0]])
+  .domain([0, (team_range[0] + 50) ])
   .range([ height, 0]);
 svg.append("g")
   .call(d3.axisLeft(y));
