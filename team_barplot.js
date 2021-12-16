ed6b7b355cab3fa0709dcae6430f49a5677bd8bb
Krzysztof Garbowicz <58812421+KrzysztofGarbowicz@users.noreply.github.com>
2021-12-16 02:06:16 +0100

    update

--- team_barplot.js
+++ team_barplot.js
@@ -67,7 +67,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 0, left: 50},
     width = 660 - margin.left - margin.right,
-    height = 750 - margin.top - margin.bottom;
+    height = 550 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
