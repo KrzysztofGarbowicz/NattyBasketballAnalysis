fada694dbe1b470453240d0683ed7b325a8b853a
Krzysztof Garbowicz <58812421+KrzysztofGarbowicz@users.noreply.github.com>
2021-12-17 13:47:05 +0100

    update

--- team_barplot.js
+++ team_barplot.js
@@ -138,7 +138,7 @@ var drawGraph = function(fileName = selectedFolder, teamName = selectedTeam) {
       .attr("text-anchor", "end")
       .attr("y", 6)
       .attr("dy", ".75em")
-      .attr("transform", "rotate(-90)")
+      .attr("transform", "rotate(90)")
       .text("Successful attempts");
   
 
