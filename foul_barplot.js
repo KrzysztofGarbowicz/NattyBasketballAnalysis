183a85fe8fd259a70dfb922bcd59d9b381924a55
Krzysztof Garbowicz <58812421+KrzysztofGarbowicz@users.noreply.github.com>
2021-12-17 14:43:48 +0100

    update

---
+++ foul_barplot.js
@@ -0,0 +1,176 @@
+// set the dimensions and margins of the graph
+var foul_margin = {top: 10, right: 30, bottom: 120, left: 80},
+foul_width = 1200 - foul_margin.left - foul_margin.right,
+foul_height = 600 - foul_margin.top - foul_margin.bottom;
+
+// append the svg object to the body of the page
+var foulSvg = d3.select("#my_foul_selection_viz")
+  .append("svg")
+    .attr("width", foul_width + foul_margin.left + foul_margin.right)
+    .attr("height", foul_height + foul_margin.top + foul_margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + foul_margin.left + "," + foul_margin.top + ")");
+
+
+// List of Teams
+var foulTeams = ["LAL", "NOP","TOR","LAC","CHI",
+"CHO","DET","IND","CLE","ORL","MIN","BRK","MEM",
+"MIA","BOS","PHI","WAS","DAL","NYK","SAS","OKC",
+"UTA","SAC","PHO","DEN","POR","ATL","MIL","HOU","GSW"]
+
+var foulTeamMenu = d3.select("#MyTeamSelection")
+
+foulTeamMenu.data(foulTeams)
+        .append("select")
+        .selectAll("option")
+        .data(foulTeams)
+       	.enter()
+        .append("option")
+        .attr("value", function(d){
+            return d;
+        })
+        .text(function(d){
+            return d;
+        })
+
+
+
+// List of groups (here I have one group per column)
+var foulTypeSelection = ["3_Pt_Shots", "2_Pt_Jump_Shots", "2_Pt_Hook_Shots", "2_Pt_Layups", "Dunks"]
+var foulTypeMenu = d3.select("#MyFoulSelection")
+
+var selectedFoul = "3_Pt_Shots"
+var selectedFoulTeam = "LAL"
+
+foulTypeMenu.data(foulTypeSelection)
+        .append("select")
+        .selectAll("option")
+        .data(foulTypeSelection)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d;
+        })
+        .text(function(d){
+            return d;
+        })
+
+foulTeamMenu.on('change', function(){
+    selectedFoulTeam = d3.select(this)
+       .select("select")
+       .property("value")
+       console.log('2222222222222')
+       console.log(selectedFoulTeam)
+       console.log('2222222222222')
+
+       drawFoulGraph(selectedFoul, selectedFoulTeam)
+});
+
+foulTypeMenu.on('change', function(){
+    selectedFoul = d3.select(this)
+       .select("select")
+       .property("value")
+       console.log('2222222222222')
+       console.log(selectedFoul)
+       console.log('2222222222222')
+
+       drawFoulGraph(selectedFoul, selectedFoulTeam)
+});        
+
+// var teamName = d3.select(teamMenu).on('change').property("value")
+// var folderName = d3.select(folderMenu).on('change').property("value")
+
+
+console.log('let\'s see foul')
+
+// Parse the Data
+var drawFoulGraph = function(foulFileName = selectedFoul, foulTeamName = selectedFoulTeam) {
+    
+    d3.selectAll("#my_foul_selection_viz > *").remove();
+
+    // Create the svg canvas in the "graph" div
+    foulSvg = d3.select("#my_foul_selection_viz")
+    .append("svg")
+      .attr("width", foul_width + foul_margin.left + foul_margin.right)
+      .attr("height", foul_height + foul_margin.top + foul_margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + foul_margin.left + "," + foul_margin.top + ")");
+            
+    d3.csv(`data/${foulFileName}/${foulTeamName}_${foulFileName}.csv`, function(foulData) {
+    
+
+    var foulTitle = `${foulFileName} : ${foulTeamName}`  
+    foulSvg.append('text')
+      .attr('class', 'title')
+      .attr('x', foul_width / 2)
+      .attr('y', 30)
+      .style("font-size", "25px")
+      .attr('text-anchor', 'middle')
+      .text(foulTitle)
+
+
+    var foulTeamRange = d3.map(foulData, function(d) { return d.NumberOfShots; }).keys()
+
+    // X axis
+    var foulX = d3.scaleBand()
+      .range([ 0, foul_width ])
+      .domain(foulData.map(function(d) { return d.Name; }))
+      .padding(0.2);
+    foulSvg.append("g")
+      .attr("transform", "translate(0," + foul_height + ")")
+      .call(d3.axisBottom(foulX))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end")
+        .style("font-size", "15px");
+
+    // Add Y axis
+    var foulY = d3.scaleLinear()
+      .domain([0, (parseInt(foulTeamRange[0]) + 20.0) ])
+      .range([ foul_height, 0])
+
+    foulSvg.append("g")
+      .call(d3.axisLeft(foulY));
+
+    foulSvg.append("text")
+      .attr("class", "y label")
+      .attr("text-anchor", "end")
+      .attr("y", 6)
+      .attr("dy", ".75em")
+      .attr("transform", "translate(-65,-10)rotate(-90)")
+      .text("Successful Attempts");
+
+
+    // Bars
+    foulSvg.selectAll("foulbar")
+      .data(foulData)
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return foulX(d.Name); })
+        .attr("y", function(d) { return foulY(d.NumberOfShots); })
+        .attr("width", foulX.bandwidth())
+        .attr("fill", "#69b3a2")
+        .style("shape-rendering", "crispEdges")
+
+
+    foulSvg.selectAll("rect")
+        .on("mouseover", function (d) {
+          d3.select(this).attr('opacity', 0.5);
+        }).on("mouseout", function (d) {
+          d3.select(this).attr('opacity', 1);
+        })
+   
+        // Animation
+    foulSvg.selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", function(d) { return foulY(d.NumberOfShots); })
+        .attr("height", function(d) { return foul_height - foulY(d.NumberOfShots); })
+
+    })  
+    
+}
+
+drawFoulGraph() 
---
+++ foul_barplot2.js
@@ -0,0 +1,176 @@
+// set the dimensions and margins of the graph
+var foul_margin2 = {top: 10, right: 30, bottom: 120, left: 80},
+foul_width2 = 1200 - foul_margin2.left - foul_margin2.right,
+foul_height2 = 600 - foul_margin2.top - foul_margin2.bottom;
+
+// append the svg object to the body of the page
+var foulSvg2 = d3.select("#my_foul_selection_viz2")
+  .append("svg")
+    .attr("width", foul_width2 + foul_margin2.left + foul_margin2.right)
+    .attr("height", foul_height2 + foul_margin2.top + foul_margin2.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + foul_margin2.left + "," + foul_margin2.top + ")");
+
+
+// List of Teams
+var foulTeams2 = ["LAL", "NOP","TOR","LAC","CHI",
+"CHO","DET","IND","CLE","ORL","MIN","BRK","MEM",
+"MIA","BOS","PHI","WAS","DAL","NYK","SAS","OKC",
+"UTA","SAC","PHO","DEN","POR","ATL","MIL","HOU","GSW"]
+
+var foulTeamMenu2 = d3.select("#MyTeamSelection2")
+
+foulTeamMenu2.data(foulTeams2)
+        .append("select")
+        .selectAll("option")
+        .data(foulTeams2)
+       	.enter()
+        .append("option")
+        .attr("value", function(d){
+            return d;
+        })
+        .text(function(d){
+            return d;
+        })
+
+
+
+// List of groups (here I have one group per column)
+var foulTypeSelection2 = ["3_Pt_Shots", "2_Pt_Jump_Shots", "2_Pt_Hook_Shots", "2_Pt_Layups", "Dunks"]
+var foulTypeMenu2 = d3.select("#MyFoulSelection2")
+
+var selectedFoul2 = "3_Pt_Shots"
+var selectedFoulTeam2 = "LAL"
+
+foulTypeMenu2.data(foulTypeSelection2)
+        .append("select")
+        .selectAll("option")
+        .data(foulTypeSelection2)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d;
+        })
+        .text(function(d){
+            return d;
+        })
+
+foulTeamMenu2.on('change', function(){
+    selectedFoulTeam2 = d3.select(this)
+       .select("select")
+       .property("value")
+       console.log('2222222222222')
+       console.log(selectedFoulTeam2)
+       console.log('2222222222222')
+
+       drawFoulGraph(selectedFoul2, selectedFoulTeam2)
+});
+
+foulTypeMenu2.on('change', function(){
+    selectedFoul2 = d3.select(this)
+       .select("select")
+       .property("value")
+       console.log('2222222222222')
+       console.log(selectedFoul2)
+       console.log('2222222222222')
+
+       drawFoulGraph(selectedFoul2, selectedFoulTeam2)
+});        
+
+// var teamName = d3.select(teamMenu).on('change').property("value")
+// var folderName = d3.select(folderMenu).on('change').property("value")
+
+
+console.log('let\'s see foul 2')
+
+// Parse the Data
+var drawFoulGraph = function(foulFileName2 = selectedFoul2, foulTeamName2 = selectedFoulTeam2) {
+    
+    d3.selectAll("#my_foul_selection_viz2 > *").remove();
+
+    // Create the svg canvas in the "graph" div
+    foulSvg2 = d3.select("#my_foul_selection_viz2")
+    .append("svg")
+      .attr("width", foul_width2 + foul_margin2.left + foul_margin2.right)
+      .attr("height", foul_height2 + foul_margin2.top + foul_margin2.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + foul_margin2.left + "," + foul_margin2.top + ")");
+            
+    d3.csv(`data/${foulFileName2}/${foulTeamName2}_${foulFileName2}.csv`, function(foulData2) {
+    
+
+    var foulTitle2 = `${foulFileName2} : ${foulTeamName2}`  
+    foulSvg2.append('text')
+      .attr('class', 'title')
+      .attr('x', foul_width2 / 2)
+      .attr('y', 30)
+      .style("font-size", "25px")
+      .attr('text-anchor', 'middle')
+      .text(foulTitle2)
+
+
+    var foulTeamRange2 = d3.map(foulData2, function(d) { return d.NumberOfShots; }).keys()
+
+    // X axis
+    var foulX2 = d3.scaleBand()
+      .range([ 0, foul_width2 ])
+      .domain(foulData2.map(function(d) { return d.Name; }))
+      .padding(0.2);
+    foulSvg2.append("g")
+      .attr("transform", "translate(0," + foul_height2 + ")")
+      .call(d3.axisBottom(foulX2))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end")
+        .style("font-size", "15px");
+
+    // Add Y axis
+    var foulY2 = d3.scaleLinear()
+      .domain([0, (parseInt(foulTeamRange2[0]) + 20.0) ])
+      .range([ foul_height2, 0])
+
+    foulSvg2.append("g")
+      .call(d3.axisLeft(foulY2));
+
+    foulSvg2.append("text")
+      .attr("class", "y label")
+      .attr("text-anchor", "end")
+      .attr("y", 6)
+      .attr("dy", ".75em")
+      .attr("transform", "translate(-65,-10)rotate(-90)")
+      .text("Successful Attempts");
+
+
+    // Bars
+    foulSvg2.selectAll("foulbar")
+      .data(foulData2)
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return foulX2(d.Name); })
+        .attr("y", function(d) { return foulY2(d.NumberOfShots); })
+        .attr("width", foulX2.bandwidth())
+        .attr("fill", "#69b3a2")
+        .style("shape-rendering", "crispEdges")
+
+
+    foulSvg2.selectAll("rect")
+        .on("mouseover", function (d) {
+          d3.select(this).attr('opacity', 0.5);
+        }).on("mouseout", function (d) {
+          d3.select(this).attr('opacity', 1);
+        })
+   
+        // Animation
+    foulSvg2.selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", function(d) { return foulY2(d.NumberOfShots); })
+        .attr("height", function(d) { return foul_height2 - foulY2(d.NumberOfShots); })
+
+    })  
+    
+}
+
+drawFoulGraph() 
